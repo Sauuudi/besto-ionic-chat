4e9cf41f919b1b909319b9fe0093f7271acbc94d
Sauuudi <iliasbox2@gmail.com>
2022-08-05 14:06:41 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'besto-ionic-chat',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
